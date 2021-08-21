b8d6d53bb243200fa2309144d5bd3911d586fce4
ipop67799 <ipop67799@gmail.com>
2021-08-21 21:02:07 +0530

    Final changes to files

---
+++ abc.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class abc {
+	public static void main (String args[]) throws IOException {
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a and b (in two separate lines): ");
+		a = sc.readLine();
+		b = sc.readLine();
+		System.out.println("Adding a and b: " + (a + b));
+	}
+}
+// This is the new line added
---
+++ def.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class def {
+    public static void main (String[] args) throws IOException {
+        String input = "qwertyuiop";
+        byte[] strAsByteArray = input.getBytes();
+        byte[] result = new byte [strAsByteArray.length];
+        for (int i = 0; i < strAsByteArray.length; i++)
+            result[i] = strAsByteArray [strAsByteArray.length - i - 1];
+        System.out.println (new String(result));
+    }
+}
+// This is the added line
